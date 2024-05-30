79bc68b6d94a7e93fb4718a0f37a439f461a9b9f
fran <franmorojo@gmail.com>
2024-05-30 12:56:44 -0400

    refactor to get more simple structur

--- .eleventy.js
+++ .eleventy.js
@@ -3,62 +3,51 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
-module.exports = function (eleventyConfig) {
+const PostCSSPlugin = require("eleventy-plugin-postcss")
+
+module.exports = function (config) {
+  // constants
+  const srcDir = "src"
+
+
   // Disable automatic use of your .gitignore
-  eleventyConfig.setUseGitIgnore(false);
+  config.setUseGitIgnore(false);
 
   // Merge data instead of overriding
-  eleventyConfig.setDataDeepMerge(true);
+  config.setDataDeepMerge(true);
 
   // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
+  config.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
     );
   });
 
   // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
+  config.addPlugin(syntaxHighlight);
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  config.addDataExtension("yaml", (contents) => yaml.load(contents));
 
-  // Copy Static Files to /_Site
-  eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
-  });
 
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
 
-  // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  // Copy Static Files to /_site
 
-  // Minify HTML
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      });
-      return minified;
-    }
+  // this copies over css files seems like
+  config.addPlugin(PostCSSPlugin)
+  config.addPassthroughCopy(`${srcDir}/**/*.js`)
+  config.addPassthroughCopy(`${srcDir}/assets`)
+  config.addPassthroughCopy(`${srcDir}/admin/config.yml`)
 
-    return content;
-  });
 
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
     dir: {
-      input: "src",
+      input: srcDir,
+      // output dir is _site/ by default
+      layouts: "_layouts",
     },
-    htmlTemplateEngine: "njk",
   };
 };
